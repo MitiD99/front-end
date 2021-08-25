9e8b5679b11b1d5154fdc8ef978af0387421caa5
Mitina Daryna <mitinadari@gmail.com>
2021-08-25 22:12:28 +0300

    new

--- js/homework_1/script_2.js
+++
@@ -1,9 +0,0 @@
-let n = prompt('Enter the number:');
-if (n < 0)
-    console.log(n + ' < 0');
-
-    else if (n > 0)
-    console.log(n + ' > 0');
-    
-    else
-    console.log(n + ' = 0');
--- js/homework_1/script_3.js
+++
@@ -1,6 +0,0 @@
-let a = prompt('Solve the equation: 8*9 = ');
-if (a == 72 )
-    console.log('Truth');
-    
-    else
-    console.log('False. Try again.');
